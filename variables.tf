a31cd4b8a9a20a52a84a9dcac0ee8b5a212f91c5
proflanky <excelj09@gmail.com>
2019-10-28 19:01:16 +0100

    terraform deployment

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  description = "aks"
+}
+
+variable "location" {
+  description = "East US"
+}
+
+variable "kubernetes_client_id" {
+  description = "762d8ab3-90a1-4acf-a74c-379cf4c2677f"
+}
+
+variable "kubernetes_client_secret" {
+  description = "Ta4ae6ec1-75bc-4e45-ba97-fae178b35ab6"
+}
