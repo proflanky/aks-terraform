e0cfc9f4f99664f58db8700bc28c093ec27449ca
Noah Abah <excelj09@gmail.com>
2021-03-29 07:29:12 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,9 +7,9 @@ variable "location" {
 }
 
 variable "kubernetes_client_id" {
-  description = "762d8ab3-90a1-4acf-a74c-379cf4c2677f"
+  description = ""
 }
 
 variable "kubernetes_client_secret" {
-  description = "Ta4ae6ec1-75bc-4e45-ba97-fae178b35ab6"
+  description = ""
 }
